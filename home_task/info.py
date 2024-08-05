192c501472a0afd23c3477f4175763f1892289ce
Akhmedov76 <ahmadovj5016@gmail.com>
2024-08-05 15:59:25 +0500

    Create chat for Twitter

---
+++ home_task/info.py
@@ -0,0 +1,13 @@
+"""
+1. https://github.com/Akhmedov76/Buying_air_tickets.git
+2. https://github.com/Akhmedov76/Hemins_for_students.git
+3. https://github.com/Akhmedov76/Rent-scooter.git
+4. https://github.com/Akhmedov76/To-Do-List.git
+
+Quyidagi vazifalarni bajaring:
+1. Git repositorysini initsializatsiya qiling.
+2. development nomli yangi branch yarating.
+3. development branchiga o'ting.
+4. todo.py nomli fayl qo'shing va vazifalarni qo'shish uchun oddiy funksiya yozing.
+5. todo.py faylini sahnaga qo'shib, commit qiling.
+6. development branchini main branchiga merge qiling. """
---
+++ lesson_5/todo.py
@@ -0,0 +1,9 @@
+"""
+Quyidagi vazifalarni bajaring:
+1. Git repositorysini initsializatsiya qiling.
+2. development nomli yangi branch yarating.
+3. development branchiga o'ting.
+4. todo.py nomli fayl qo'shing va vazifalarni qo'shish uchun oddiy funksiya yozing.
+5. todo.py faylini sahnaga qo'shib, commit qiling.
+6. development branchini main branchiga merge qiling. """
+
